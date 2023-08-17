77c745da078a3ad12264b12b32c69ee5227926e1
Guilherme Capelli <129558769+GuilhermeCapelli@users.noreply.github.com>
2023-08-17 20:08:43 -0300

    decimo segundo commit

--- src/pessoa/dto/Cadastropessoa.dto.ts
+++ src/pessoa/dto/Cadastropessoa.dto.ts
@@ -5,4 +5,19 @@ export class CriaPessoaDTO{
     @IsString()
     @IsNotEmpty({message: "Nome não pode ser vazio"})
     NOME: string;
+
+    @IsString()
+    @IsNotEmpty({message: "Endereço não pode ser vazio"})
+    ENDERECO: string;
+
+    
+    @IsString()
+    @IsNotEmpty({message: "Telefone não pode ser vazio"})
+    TELEFONE: string;
+
+    
+    @IsString()
+    @IsNotEmpty({message: "Nome do Cachorro não pode ser vazio"})
+    NOME_CACHORRO: string;
+
 }
